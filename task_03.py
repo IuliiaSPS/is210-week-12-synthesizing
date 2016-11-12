853b657c54c4d8e3a83776d722fb0497b2a259de
Iuliia Ambrokh <iuliia.ambrokh@spsmail.cuny.edu>
2016-11-12 21:02:53 +0000

    Task_03.py

--- task_03.py
+++ task_03.py
@@ -2,12 +2,26 @@
 # -*- coding: utf-8 -*-
 """Task 03 module"""
 
+
 def exception_test(arg1, arg2, arg3):
+    """Function to do some evaluation.
+
+    Args:
+        arg1(optional): user input.
+        arg2(optional): user input.
+        arg3(optional): user input.
+
+    Returns:
+        bool: boolean value if inputs have the same data type.
+
+    Examples:
+        >>> exception_test(['apple'], 0, 'p')
+        False
+    """
     caught = False
     try:
         arg1[arg2].index(arg3)
-    except:
+    except (TypeError, KeyError, IndexError):
         caught = True
 
     return caught
-    
